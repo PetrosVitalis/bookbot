2afa8539ad05808c0281d8042d10b6b6d482e143
PetrosVitalis <pvitalis1118@gmail.com@gmail.com>
2025-07-14 21:20:55 +0300

    add new prompt

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def main():
 
     client = genai.Client(api_key=api_key)
     
-    res = client.models.generate_content(model="gemini-2.0-flash-001", contents="Does the rise of AI make it a bad idea to become backend developer? One paragraph.")
+    res = client.models.generate_content(model="gemini-2.0-flash-001", contents="Does the rise of AI make it a bad idea to become developer? One paragraph.")
     print(res.text)
     print("Prompt tokens: ", res.usage_metadata.prompt_token_count)
     print("Response tokens: ", res.usage_metadata.candidates_token_count)
