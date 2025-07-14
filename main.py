8450fc043724cdff6fc6fad2e322b922d700afe8
PetrosVitalis <pvitalis1118@gmail.com@gmail.com>
2025-07-14 21:29:08 +0300

    cli prompt

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 def main():
     import os
     from dotenv import load_dotenv
+    import sys
 
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
@@ -8,8 +9,13 @@ def main():
     from google import genai
 
     client = genai.Client(api_key=api_key)
+    if len(sys.argv) < 2:
+        print("Usage: python main.py <prompt>")
+        sys.exit(1)
+    prompt = sys.argv[1]
     
-    res = client.models.generate_content(model="gemini-2.0-flash-001", contents="Does the rise of AI make it a bad idea to become developer? One paragraph.")
+    
+    res = client.models.generate_content(model="gemini-2.0-flash-001", contents=prompt)
     print(res.text)
     print("Prompt tokens: ", res.usage_metadata.prompt_token_count)
     print("Response tokens: ", res.usage_metadata.candidates_token_count)
